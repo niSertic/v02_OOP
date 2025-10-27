714cc6503a2f9f101403dd167a9a55f1bb13354e
Nikola <nisertic@vsite.hr>
2025-10-27 20:28:06 +0100

    all done

--- app/main.cpp
+++ app/main.cpp
@@ -2,6 +2,21 @@
 #include <format>
 #include "app.h"
 
+using namespace vsite::oop::v2;
+
 int main()
 {
+	double r, g, b;
+
+	std::cout << "\nEnter RGB values [0.0 - 1.0] :" << std::endl;
+	std::cout << "Red: " << std::endl;
+	std::cin >> r;
+	std::cout << "Green: " << std::endl;
+	std::cin >> g;
+	std::cout << "Blue: " << std::endl;
+	std::cin >> b;
+
+	color color01 = color(r, g, b);
+	std::cout << "\nColor RGB values:" << color::to_hex(color01.get_color_ref()) << std::endl;
+
 }
